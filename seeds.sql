40f54fa8cf7bc30cbdb0ffda88b46d2dc24a371f
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-18 23:44:29 -0400

    seeds updated

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+USE employee_management;
+
+INSERT INTO department (name) VALUES ('Web Development');
+
+INSERT INTO role (title, salary) VALUES ('Senior Developer', 120000);
+
+INSERT INTO employee (first_name, last_name) VALUES ('Chad', 'Laflamme');

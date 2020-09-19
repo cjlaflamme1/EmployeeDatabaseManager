ece95190c0501606fd7af8a663c507834c08b260
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-19 13:28:23 -0400

    final schema and seeds edit

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS employee_management;
+
 CREATE DATABASE employee_management;
 USE employee_management;
 
@@ -12,8 +14,8 @@ CREATE TABLE role
 (
 	id int NOT NULL AUTO_INCREMENT,
 	title varchar(30) NOT NULL,
-    salary DECIMAL(10,2) NULL,
-    department_id INT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
 	PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -23,7 +25,7 @@ CREATE TABLE employee
 	id int NOT NULL AUTO_INCREMENT,
 	first_name varchar(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT Null,
+    role_id INT NOT Null,
     manager_id INT NULL,
 	PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id),
--- seeds.sql
+++ seeds.sql
@@ -2,6 +2,6 @@ USE employee_management;
 
 INSERT INTO department (name) VALUES ('Web Development');
 
-INSERT INTO role (title, salary) VALUES ('Senior Developer', 120000);
+INSERT INTO role (title, salary, department_id) VALUES ('Senior Developer', 120000, 1);
 
-INSERT INTO employee (first_name, last_name) VALUES ('Chad', 'Laflamme');
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Chad', 'Laflamme', 1);
